e181b1708130c4485fb969e9d985c57c22f9189e
Mohan Krishna A <mohankrishna.a757@gmail.com>
2025-04-22 12:19:53 +0530

    Add ClientPostsList and PostsPage components for fetching and displaying posts

---
+++ app/posts/page.tsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react';
+import ClientPostsList from './components/ClientPostsList';
+
+const PostsPage = () => {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ClientPostsList />
+    </div>
+  );
+};
+
+export default PostsPage;
